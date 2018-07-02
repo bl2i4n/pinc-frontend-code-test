8f34a3553678050ebdf743b699f214800364d448
bl2i4n <bl2i4n@yahoo.com>
2018-07-01 21:19:09 -0700

    feat: add react-bootstrap and navbar, needs styling'

--- pinc/src/App.js
+++ pinc/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+    <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#home">React-Bootstrap</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          Link
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          Link
+        </NavItem>
+      </Nav>
+    </Navbar>
+    
+    </div>
     );
   }
 }
